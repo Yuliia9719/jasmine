bd234b46d67ebd05f72be2b88ac05c37f2482d37
Yuliia <iyulia1997len@gmail.com>
2024-02-08 01:46:18 +0200

    added git ignore + modified spec2

--- spec/main.spec2.js
+++ spec/main.spec2.js
@@ -1,33 +1,33 @@
-describe("Checking the week", () => {
-  it("week 1: Понедельник", () => {
+describe("Checking the week day", () => {
+  it("day of the week 1: Понедельник", () => {
     expect(weekFn(1)).toBe("Понедельник");
   });
 
-  it("week 2: Вторник", () => {
+  it("day of the week 2: Вторник", () => {
     expect(weekFn(2)).toBe("Вторник");
   });
 
-  it("week 3: Среда", () => {
+  it("day of the week 3: Среда", () => {
     expect(weekFn(3)).toBe("Среда");
   });
 
-  it("week 4: Четверг", () => {
+  it("day of the week 4: Четверг", () => {
     expect(weekFn(4)).toBe("Четверг");
   });
 
-  it("week 5: Пятница", () => {
+  it("day of the week 5: Пятница", () => {
     expect(weekFn(5)).toBe("Пятница");
   });
 
-  it("week 6: Суббота", () => {
+  it("day of the week 6: Суббота", () => {
     expect(weekFn(6)).toBe("Суббота");
   });
 
-  it("week 7: Воскресенье", () => {
+  it("day of the week 7: Воскресенье", () => {
     expect(weekFn(7)).toBe("Воскресенье");
   });
 
-  it("any other number of the week: null", () => {
+  it("any other number of the day: null", () => {
     expect(weekFn(66)).toBe(null);
   });
 });
